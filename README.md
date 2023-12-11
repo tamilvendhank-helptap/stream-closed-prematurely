d76b860cba675a6eabab2c95d7c60d8382c2c29b
Tamil Vendhan Kanagarasu <tamil@helptap.com>
2023-12-11 09:47:28 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# stream-closed-prematurely
+
+Reproducing of "stream closed prematurely"
+
+## Installation
+
+```
+git clone git@github.com:tamilvendhank-helptap/stream-closed-prematurely.git
+cd stream-closed-prematurely
+npm install
+```
+
+## Issue
+
+Visit: http://127.0.0.1:3456 
---
+++ index.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+  const stream = fs.createReadStream('./package.json', 'utf8')
+  reply.header('Content-Type', 'application/octet-stream')
+  reply.send(stream);
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3456 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
